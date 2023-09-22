95d73a9a77e227f505a2b8436a30f1d0d9b91c6c
MykhailoSalivonov <smisha87@gmail.com>
2023-09-22 23:25:57 +0300

    added module testing project

---
+++ ModuleTesting/ModuleTesting/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ModuleTesting
+{
+    public class Triangle
+    {
+        public Triangle(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentException("Each side of triangle should be bigger than zero.");
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+                throw new ArgumentException("Sum of any two side should be bigger than third one.");
+
+            A = a; B = b; C = c;
+
+            if (A == B && A == C && B == C)
+                Type = TriangleType.Equilateral;
+            else if (A == C || B == C || A == B)
+                Type = TriangleType.Isosceles;
+            else
+                Type = TriangleType.Scalene;
+        }
+
+        public int A { get; }
+        public int B { get; }
+        public int C { get; }
+
+        public TriangleType Type {  get; }
+    }
+
+    public enum TriangleType
+    {
+        Scalene,
+        Isosceles,
+        Equilateral
+    }
+}
---
+++ ModuleTesting/UnitTestsForLab1/UnitTests/IncorrectTriangleUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using ModuleTesting;
+
+namespace UnitTestsForLab1.UnitTests
+{
+    internal class IncorrectTriangleUnitTest
+    {
+        [Test]
+        public void TriangleWithZeroSide()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(0, 2, 3), "Triangle was created with side equals to 0.");
+        }
+
+        [Test]
+        public void TriangleWithSideMuchBiggerThenOther() 
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(1, 2, 5), "Triangle was created with one side much bigger than two others.");
+        }
+
+        [Test]
+        public void TriangleWithNegativeSide()
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(-1, 2, 2), "Triangle was created with one side with negative value.");
+        }
+    }
+}
