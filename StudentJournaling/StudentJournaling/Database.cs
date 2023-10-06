410b9aceb92865e87626621e024cbfac7dbe6222
MykhailoSalivonov <smisha87@gmail.com>
2023-10-06 10:56:34 +0300

    journal

---
+++ StudentJournaling/StudentJournaling/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentJournaling
+{
+    internal class Database
+    {
+        string dbFilePath = "D:\\Git\\Studying\\StudentJournaling\\db\\database.db";
+
+
+        public Database() 
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFilePath};Version=3;"))
+            {
+                connection.Open();
+
+                // Тут ви можете створити таблиці і виконувати інші операції з базою даних
+            }
+        }
+    }
+}
---
+++ StudentJournaling/total.sql
@@ -0,0 +1,20 @@
+--
+select '551' as grp,'salivonov'    as nm, 0+1 as ball
+
+union
+select '551'as grp,'zas4ik' as nm, 0 +2.5 as ball
+
+
+;
+
+
+
+
+
+
+
+
+
+
+
+;
