0412879827275aaeb326a3d48e30782024be56da
Mykhailo Salivonov <8596687@stud.nau.edu.ua>
2023-11-21 13:44:05 +0200

    unit

---
+++ ModuleTesting/UnitTestsLab1/GeographicCalculatorUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using ModuleTesting;
+using NUnit.Framework;
+
+namespace UnitTestsLab2
+{
+    public class GeographicCalculatorUnitTests
+    {
+        [Test]
+        public void Test1()
+        {
+            List<double> results = new List<double>();
+            var latitude = 47;
+            for (double i = 0; i < 0.001; i+= double.Epsilon)
+            {
+                results.Add(GeographicCalculator.MeridianLength(latitude, i));
+            }
+
+            List<double> sortedResult = results.OrderBy(x=>x).ToList();
+            CollectionAssert.AreEqual(sortedResult, results);
+
+        }
+    }
+}
