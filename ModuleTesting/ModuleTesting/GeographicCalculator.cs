d81c31f8480bc36a28878534e3027353271a8c31
Mykhailo Salivonov <8596687@stud.nau.edu.ua>
2023-12-01 13:36:54 +0200

    unit tests

--- ModuleTesting/ModuleTesting/GeographicCalculator.cs
+++ ModuleTesting/ModuleTesting/GeographicCalculator.cs
@@ -12,8 +12,9 @@ namespace ModuleTesting
         public const double sb = b * b;
         public const double seccentricity = (sa - sb) / sa;
         public const double seccentricity2 = (sa - sb) / sb;
-        /// підрахунок довжини в км. до одного градусу на меридіані
         
+        
+        /// підрахунок довжини в км. до одного градусу на меридіані
         public static double MeridianLength(double lattitude, double d = 1.0)
         {
             if (d > 1) 
@@ -26,13 +27,9 @@ namespace ModuleTesting
             else if (lattitude < 0.0) 
                 lattitude = 0.0;
 
-            double lttM = lattitude + d / 2.0;
-            // взяли середину між нижньою і верхньою частиною дуги
-            // double eccentricity = Math.Sqrt ( seccentricity );
+            double lttM = lattitude + ((double)d) / 2.0;
             double sinB = sin(lttM);
             double foo = Math.Sqrt(1 - seccentricity * sinB * sinB);
-            //double N = a / ( Math.Sqrt (foo));
-            // радиус кривизни першого вертикалу
             double M = a * (1 - seccentricity)
                 / (foo * foo * foo);
             double rc = M * (Math.PI / 180.0); // length of 1 degree
--- ModuleTesting/UnitTestsLab1/GeographicCalculatorUnitTests.cs
+++ ModuleTesting/UnitTestsLab1/GeographicCalculatorUnitTests.cs
@@ -1,23 +1,48 @@
 ﻿using ModuleTesting;
 using NUnit.Framework;
 
-namespace UnitTestsLab2
+namespace UnitTestsLab6
 {
     public class GeographicCalculatorUnitTests
     {
+        int latitude = 47;
+
         [Test]
-        public void Test1()
+        public void ContinuityNearZeroDegree()
         {
-            List<double> results = new List<double>();
-            var latitude = 47;
-            for (double i = 0; i < 0.001; i+= double.Epsilon)
+            List<double> resultList = new List<double>();
+
+            for (double i = 0.0; i < double.Epsilon*1000; i+= double.Epsilon)
             {
-                results.Add(GeographicCalculator.MeridianLength(latitude, i));
+                resultList.Add(GeographicCalculator.MeridianLength(latitude, i));
+            }
+            List<double> sortedResult = resultList.OrderBy(x=>x).ToList();
+            CollectionAssert.AreEqual(sortedResult, resultList);
+
+        }
+
+        [Test]
+        public void ContinuityNearOneDegree()
+        {
+            List<double> resultList = new List<double>();
+
+            for (double i = 0.0; i < double.Epsilon * 1000; i += double.Epsilon)
+            {
+                resultList.Add(GeographicCalculator.MeridianLength(latitude, 1-i));
             }
 
-            List<double> sortedResult = results.OrderBy(x=>x).ToList();
-            CollectionAssert.AreEqual(sortedResult, results);
+            List<double> sortedResult = resultList.OrderBy(x => x).ToList();
+            CollectionAssert.AreEqual(sortedResult, resultList);
+        }
 
+        [Test]
+        public void ContinuityOneDegree()
+        {
+            double preaviousValue = 0.0;
+            double currentValue;
+
+            currentValue = GeographicCalculator.MeridianLength(latitude, 1);
+            Assert.Greater(currentValue, preaviousValue);
         }
     }
 }
