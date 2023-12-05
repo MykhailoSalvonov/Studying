6b726f1fe382aff3344c062f70a9e2fce853684a
MykhailoSalivonov <smisha87@gmail.com>
2023-12-06 00:51:16 +0200

    contrast

--- Coursework/PythonScripts/Filtering.py
+++ Coursework/PythonScripts/Filtering.py
@@ -1,31 +1,11 @@
 import cv2
-import numpy as np
 import sys
-
-y1 = np.array([
-      [3.75457E-09, 8.93587E-07, 5.40282E-06, -7.38748E-05, 5.40282E-06, 8.93587E-07, 3.75457E-09],
-      [8.93587E-07, 2.12674E-05, 1.285871E-03, -1.758221E-02, 1.285871E-03, 2.12674E-05, 8.93587E-07],
-      [5.40282E-06, 1.285871E-03, 7.774658E-03, -1.06305883E-01, 7.774658E-03, 1.285871E-03, 5.40282E-06],
-      [-7.38748E-05, -1.758221E-02, -1.06305883E-01, 1.45356119, -1.06305883E-01, -1.758221E-02, -7.38748E-05],
-      [5.40282E-06, 1.285871E-03, 7.774658E-03, -1.06305883E-01, 7.774658E-03, 1.285871E-03, 5.40282E-06],
-      [8.93587E-07, 2.12674E-05, 1.285871E-03, -1.758221E-02, 1.285871E-03, 2.12674E-05, 8.93587E-07],
-      [3.75457E-09, 8.93587E-07, 5.40282E-06, -7.38748E-05, 5.40282E-06, 8.93587E-07, 3.75457E-09]
-])
-
-y2 =  np.array([
-      [1.24562E-08, 2.96456E-06, 1.59314E-05, -0.000149424, 1.59314E-05, 2.96456E-06, 1.24562E-08],
-      [2.96456E-06, 0.000705566, 0.003791678, -0.035562919, 0.003791678, 0.000705566, 2.96456E-06],
-      [1.59314E-05, 0.003791678, 0.020376288, -0.191113331, 0.020376288, 0.003791678, 1.59314E-05],
-      [-0.000149424, -0.035562919, -0.191113331, 1.792490633, -0.191113331, -0.035562919, -0.000149424],
-      [1.59314E-05, 0.003791678, 0.020376288, -0.191113331, 0.020376288, 0.003791678, 1.59314E-05],
-      [2.96456E-06, 0.000705566, 0.003791678, -0.035562919, 0.003791678, 0.000705566, 2.96456E-06],
-      [1.24562E-08, 2.96456E-06, 1.59314E-05, -0.000149424, 1.59314E-05, 2.96456E-06, 1.24562E-08]
-])
+import Filters
 
 
 def apply_filter_to_image(image_path):
     image = cv2.imread(image_path)
-    filter_matrix = y1
+    filter_matrix = Filters.y4
 
     if image is None:
         raise ValueError(f"Не вдалося завантажити зображення за шляхом {image_path}")
@@ -38,7 +18,10 @@ def apply_filter_to_image(image_path):
 
     filtered_image = cv2.merge([blue_filtered, green_filtered, red_filtered])
 
-    output_path = image_path.replace('.jpg', '_filtered.jpg')
+    if '_filtered.jpg' in image_path:
+        output_path = image_path
+    else:
+        output_path = image_path.replace('.jpg', '_filtered.jpg')
     cv2.imwrite(output_path, filtered_image)
 
     return output_path
@@ -48,9 +31,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
 
-    print("I'M HERE!!!!")
-    image_path = sys.argv[1]
-    output_path = apply_filter_to_image(image_path)
+    img_path = sys.argv[1]
+    out_path = apply_filter_to_image(img_path)
 
-    print(output_path)
+    print(out_path)
 
--- Coursework/UserInterface/MainView.cs
+++ Coursework/UserInterface/MainView.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
@@ -32,31 +33,13 @@ namespace UserInterface
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     string filePath = openFileDialog.FileName;
-                    pictureBox1.Image = System.Drawing.Image.FromFile(filePath);
-                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    //pictureBox1.Image = System.Drawing.Image.FromFile(filePath);
+                    //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
 
-                    pictureBox1.Size = pictureBox1.Image.Size;
+                    //pictureBox1.Size = pictureBox1.Image.Size;
+                    //Size = new Size(pictureBox1.Image.Width + 40, pictureBox1.Image.Height + 60);
 
-                    // Встановити розмір форми. Додати маржини для рамок вікна та заголовка.
-                    this.Size = new Size(pictureBox1.Image.Width + 40, pictureBox1.Image.Height + 60);
-
-                    Data = BSplineCore.ImageConvertor.ConvertImageTo3DArray(filePath);
-                    // Створення нового процесу для запуску Python скрипта
-                    ProcessStartInfo start = new ProcessStartInfo();
-                    start.FileName = "C:\\Users\\saliv\\anaconda3\\envs\\desctop_env\\python.exe"; // або шлях до python.exe, якщо 'python' не у змінній середовища PATH
-                    start.Arguments = $"\"D:\\Git\\Studying\\Coursework\\PythonScripts\\Filtering.py\" \"{filePath}\"";
-                    start.UseShellExecute = false; // Не використовуй
-                    start.RedirectStandardOutput = true; // Дозволить читання стандартного виводу
-
-                    using (Process process = Process.Start(start))
-                    {
-                        using (StreamReader reader = process.StandardOutput)
-                        {
-                            // Читаємо стандартний вивід Python скрипта (шлях до нового файлу)
-                            string result = reader.ReadToEnd();
-                            Console.WriteLine(result);
-                        }
-                    }
+                    filePath = UseFilter(filePath);
                 }
             }
         }
@@ -64,6 +47,26 @@ namespace UserInterface
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private string UseFilter(string filePath)
+        {
+            string resultPath;
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = "C:\\Users\\saliv\\anaconda3\\envs\\desctop_env\\python.exe";
+            start.Arguments = $"\"D:\\Git\\Studying\\Coursework\\PythonScripts\\Filtering.py\" \"{filePath}\"";
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    resultPath = reader.ReadToEnd();
+                }
+            }
+
+            return resultPath;
         }
     }
 }
