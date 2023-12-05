edde69b51b8b2e1121de76848a8b27e7cff7ab21
MykhailoSalivonov <smisha87@gmail.com>
2023-12-05 23:35:08 +0200

    filtering

--- Coursework/UserInterface/MainView.cs
+++ Coursework/UserInterface/MainView.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +41,22 @@ namespace UserInterface
                     this.Size = new Size(pictureBox1.Image.Width + 40, pictureBox1.Image.Height + 60);
 
                     Data = BSplineCore.ImageConvertor.ConvertImageTo3DArray(filePath);
+                    // Створення нового процесу для запуску Python скрипта
+                    ProcessStartInfo start = new ProcessStartInfo();
+                    start.FileName = "C:\\Users\\saliv\\anaconda3\\envs\\desctop_env\\python.exe"; // або шлях до python.exe, якщо 'python' не у змінній середовища PATH
+                    start.Arguments = $"\"D:\\Git\\Studying\\Coursework\\PythonScripts\\Filtering.py\" \"{filePath}\"";
+                    start.UseShellExecute = false; // Не використовуй
+                    start.RedirectStandardOutput = true; // Дозволить читання стандартного виводу
+
+                    using (Process process = Process.Start(start))
+                    {
+                        using (StreamReader reader = process.StandardOutput)
+                        {
+                            // Читаємо стандартний вивід Python скрипта (шлях до нового файлу)
+                            string result = reader.ReadToEnd();
+                            Console.WriteLine(result);
+                        }
+                    }
                 }
             }
         }
